ce5acbd4a1826a5c96c71eaf5ea609fa450abd50
mursts <mursts@gmail.com>
2018-06-17 23:57:18 +0900

    日付登録状況を返すAPIを作成

--- receiver.py
+++ receiver.py
@@ -2,15 +2,16 @@
 # coding: utf-8
 
 import datetime
+import re
 
 import webapp2
+from google.appengine.ext import ndb
 
 from model import Receive
 
 
 class ReceiveHandler(webapp2.RequestHandler):
     def get(self):
-
         header = self.request.headers.get("X-LUNCH-ALERT", None)
         if header is None:
             return
@@ -22,6 +23,38 @@ class ReceiveHandler(webapp2.RequestHandler):
         receive.put()
 
 
+rgx = re.compile('\d{4}-\d{2}-\d{2}')
+
+
+class ReceiveAPIHandler(webapp2.RequestHandler):
+    def get(self):
+        params = self.request.GET
+
+        if 'date' not in params:
+            self.response.status = 400
+            self.response.write('Bad Request')
+            return
+
+        d = params['date']
+
+        if not rgx.match(d):
+            self.response.status = 400
+            self.response.write('Bad Request')
+            return
+
+        receive_key = ndb.Key("Receive", d)
+        receive = receive_key.get()
+
+        if receive is None:
+            self.response.status = 400
+            self.response.write('Bad Request')
+            return
+
+        self.response.status = 200
+        self.response.write(params['date'])
+
+
 app = webapp2.WSGIApplication([
     ('/receive', ReceiveHandler),
+    ('/api/receive', ReceiveAPIHandler),
 ], debug=False)
